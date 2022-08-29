c57fbfa64b7c006281744c9f9e5a827c3d0a642c
davi <davi.ghiggino@concilie.com.br>
2022-08-29 00:28:26 -0300

    test: Implementando os testes da entidade Cupom

--- tests/test_coupon.py
+++ tests/test_coupon.py
@@ -5,16 +5,10 @@ def test_deve_criar_um_cupom():
 def test_deve_aplicar_deconto_do_coupon(): 
     coupon = Coupon('VALE10', 10, '2032-12-20T10:00:00')
     total = 100    
-    new_total = total - coupon.getDiscount(total)
+    new_total = total - coupon.get_discount(total)
     assert new_total == 90
 
 def test_deve_criar_cupon_expirado(): 
     coupon = Coupon('VALE10', 10, '2032-04-20T10:00:00')
-    is_expired = coupon.isExpired() 
-    assert is_expired == False   
-
-def test_deve_retornar_o_valor_integral_da_compra_cupons_expirados():
-    coupon = Coupon('VALE10', 10, '2002-12-20T10:00:00')
-    total = 100    
-    new_total = total - coupon.getDiscount(total)
-    assert new_total == 100
+    is_expired = coupon.is_expired("2042-03-05T10:00:00") 
+    assert is_expired == True
--- tests/test_order.py
+++ tests/test_order.py
@@ -1,3 +1,4 @@
+from source.coupon import Coupon
 from source.cpf import Cpf
 from source.order import Order
 from source.item import Item
@@ -5,7 +6,9 @@ from source.item import Item
 def test_deve_criar_um_pedido():
     cpf = Cpf("879.444.500-12")
     order = Order(cpf)
-    total = order.get_quantity_items()
+    quant_items = order.get_quantity_items()
+    total = order.get_total()
+    assert quant_items == 0
     assert total == 0
 
 def test_deve_adicionar_um_item_com_quantidade_negativa():
@@ -23,3 +26,23 @@ def test_deve_aumentar_a_quantidade_ao_inserir_itens_repetidos():
     order.add_item(Item(),2)
     assert order.get_quantity_items() == 1
     assert order.order_items[0].quantity == 3
+
+def test_deve_gerar_erro_ao_inserir_dois_cupons_no_pedido():
+    cpf = Cpf("879.444.500-12")
+    order = Order(cpf=cpf)
+    coupon = Coupon('VALE10', 10, "2023-04-20T10:00:00")
+    coupon2 = Coupon('VALE20', 20, "2024-04-20T10:00:00")
+    try:
+        order.add_coupon(coupon)
+        order.add_coupon(coupon2)
+    except Exception as exception:
+        assert "there is coupon in order" in str(exception)
+
+def test_deve_gerar_erro_ao_inserir_coupon_invalido():
+    cpf = Cpf("879.444.500-12")
+    order = Order(cpf=cpf)
+    coupon = Coupon('VALE10', 10, "2012-04-20T10:00:00")
+    try:
+        order.add_coupon(coupon)
+    except Exception as exception:
+        assert "coupon invalid" in str(exception)
