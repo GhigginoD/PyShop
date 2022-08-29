3228c5638f90bca24401d198a236c67165af591c
davi <davi.ghiggino@concilie.com.br>
2022-08-29 00:26:39 -0300

    feat: Implementando custo dos itens no carrinho da compra

--- source/orderItem.py
+++ source/orderItem.py
@@ -1,6 +1,10 @@
 from .item import Item
 class OrderItem():
-    def __init__(self, item:Item, quantity:int):
-         if quantity <= 0: raise Exception("negative quantity")
-         self.item = item
-         self.quantity = quantity
+    def __init__(self, id_item:int, price:float, quantity:int):
+        if quantity <= 0: raise Exception("negative quantity")
+        self.id_item = id_item
+        self.price = price
+        self.quantity = quantity
+
+    def get_total(self):
+        return self.price * self.quantity
