2ef85c97beb2ff483fad695cab8b9da9bb14c22c
davi <davi.ghiggino@concilie.com.br>
2022-08-29 00:28:59 -0300

    feat: Implementação do cupom

--- source/coupon.py
+++ source/coupon.py
@@ -1,15 +1,13 @@
 from datetime import datetime
 class Coupon:
-
-    def __init__(self, code:str, percentage:int, expiredDate:str) -> None:
+    def __init__(self, code:str, percentage:int, expired_date:str) -> None:
         self.__percentage = percentage
         self.__code = code
-        self.__expiredDate = expiredDate
+        self.__expired_date = expired_date
 
-    def getDiscount(self, total:int)->int:
-        if self.isExpired(): return 0
+    def get_discount(self, total:int) -> int:
         return (total * self.__percentage) / 100
 
-    def isExpired(self):
-        return datetime.now() > (datetime.strptime(self.__expiredDate,'%Y-%m-%dT%H:%M:%S'))
+    def is_expired(self, date: str) -> bool:
+        return datetime.strptime(date,'%Y-%m-%dT%H:%M:%S') > datetime.strptime(self.__expired_date,'%Y-%m-%dT%H:%M:%S')
           
